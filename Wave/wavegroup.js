1730e7c5125a695f9162a3e8d46a5f9aa8097f9d
unknown <mkonjkr@gmail.com>
2021-08-13 16:14:09 -0700

    #revised

--- Wave/wavegroup.js
+++ Wave/wavegroup.js
@@ -5,9 +5,10 @@ import {
 export class WaveGroup {
      constructor() {
          this.totalWaves = 3;
+         // wave changes depends on totalPoints
          this.totalPoints = 5;
 
-         this.color = ['rgba(0,199,235,0.4)', 'rgba(0,146,199,0.4)', 'rgba(0,88,158,0.4)'];
+         this.color = ['rgba(255,0,0,0.4)', 'rgba(255,255,0,0.4)', 'rgba(0,255,255,0.4)'];
 
          this.waves = [];
 
